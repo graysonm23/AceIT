93d28ea836a43973ad2b00849aca37d48db1ce6a
Khadijah Grimes <khadijahgrimes7@gmail.com>
2020-02-15 13:47:35 -0600

    updated card component

---
+++ src/components/card/card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const ImgCard = (props) => {
+    return (
+        <div>
+            <Card>
+                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+
+export default ImgCard;
