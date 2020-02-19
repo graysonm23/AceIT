b5e2c3aa1ea1608b1feb39d560c2a125fbe5feb3
amanda <amanda.m.kunz@gmail.com>
2020-02-18 22:46:27 -0600

    adding user controller

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const db = require("../models");
+
+// Defining methods for the UserController
+module.exports = {
+  findAll: function(req, res) {
+    db.User.find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.User.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then(dbModel => {
+        console.log("This is res.body: ", res.body);
+        console.log("This is req.body: ", req.body);
+        console.log("This is dbModel: ", dbModel);
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+};
