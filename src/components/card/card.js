ce494aae516dd9e3f5a76d269a6e9489f927ff34
Khadijah Grimes <khadijahgrimes7@gmail.com>
2020-02-20 20:26:57 -0600

    updated the card components

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,21 +3,110 @@ import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button
 } from 'reactstrap';
+import './card.css';
+import "../navbar/navbar";
+import "../navbar/index";
+import icon from "../../images/activityIcon.png";
+import icon2 from "../../images/goalicon.png";
+import icon3 from "../../images/homeicon.png";
 
 const ImgCard = (props) => {
     return (
-        <div>
-            <Card>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <Button>Button</Button>
-                </CardBody>
-            </Card>
+        <div class="card">
+            <div class="container">
+
+                <Card>
+                    <CardImg top width="35%" src={icon2} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Goals</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText> </CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardImg top width="35%" src={icon} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle> Activity</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+
+
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
+                    <CardBody>
+                        <CardTitle>Home</CardTitle>
+                        <CardSubtitle></CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Click</Button>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
+
+
     );
+
 };
 
 
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,3 @@
-import React from "react";
+export { default } from "./navbar";
 
-function Nav() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="/">
-                React Reading List
-        </a>
-        </nav>
-    );
-}
 
-export default Nav;
