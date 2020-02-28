23b7b32f4af6aef8d7cc753a74205b7144b35eea
Jude <judecarpenter@ymail.com>
2020-02-27 21:52:47 -0600

    finished the signup page and styling

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -29,19 +29,26 @@ function Signup() {
             <Card className="homeCard">
               <CardBody>
                 <CardTitle className="homeCardTitle">
-                  <br></br>
-                  <br></br>
-                  <h1>Signup</h1>
+                  <h1 className="signupTitle">Signup</h1>
                 </CardTitle>
                 <CardBody className="homeCardBody">
                   <form onSubmit={handleEmailSubmit}>
                     <FormGroup>
-                      <label for="Your Email">
-                        <br></br>
-                        <h4>Enter your Email</h4>{" "}
+                      <label for="Your Name">
+                        <h4 className="signupSubtitle">Enter your Name</h4>{" "}
                       </label>
-                      <br></br>
-                      <br></br>
+                      <input
+                        className="col s-12 signup-input"
+                        type="Name"
+                        name="name"
+                        id="exampleName"
+                        placeholder="John Doe"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <label for="Your Email">
+                        <h4 className="signupSubtitle">Enter your Email</h4>{" "}
+                      </label>
                       <input
                         className="col s-12 signup-input"
                         type="email"
@@ -51,6 +58,26 @@ function Signup() {
                       />
                     </FormGroup>
                     <br></br>
+                    <FormGroup>
+                      <label for="Your Email">
+                        <h4 className="signupSubtitle">Enter your Password</h4>{" "}
+                      </label>
+                      <input
+                        className="col s-12 signup-input"
+                        type="password"
+                        name="ExamplePassword"
+                        id="examplePassword"
+                        placeholder="Your Password Here"
+                      />
+                      <br></br>
+                      <input
+                        className="col s-12 signup-input"
+                        type="confirmPassword"
+                        name="ConfirmPassword"
+                        id="CondirmPassword"
+                        placeholder="Confirm Password"
+                      />
+                    </FormGroup>
                     <Button id="buttonCancel" size="lg" color="danger">
                       X
                     </Button>{" "}
