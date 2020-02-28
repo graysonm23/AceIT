94eaf9385bfd31d77f432f00993405a3c60dba68
Khadijah Grimes <khadijahgrimes7@gmail.com>
2020-02-27 21:02:35 -0600

    updated the card page/ dynamic layout.

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -6,105 +6,78 @@ import {
     CardBody,
     CardTitle,
     CardSubtitle,
-    Button
+    Button,
+    Row,
+    Col
 } from "reactstrap";
-import "./card.css";
+// import "./card.css";
 import "../navbar/navbar";
 import "../navbar/index";
 import icon from "../../images/activityIcon.png";
 import icon2 from "../../images/goalicon.png";
 import icon3 from "../../images/homeicon.png";
+import icon4 from "../../images/yes-icon.png";
+import icon5 from "../../images/no-icon.png";
+import icon6 from "../../images/run-icon.png";
+import icon7 from "../../images/music-icon.png";
+import icon8 from "../../images/drink-icon.png";
+
+
+const dummyArr = [
+    {
+        icon: icon,
+        title: "Activity"
+    },
+    {
+        icon: icon2,
+        title: "Goals"
+    },
+    {
+        icon: icon3,
+        title: "Home"
+    },
+    {
+        icon: icon4,
+        title: "Yes"
+    },
+    {
+        icon: icon5,
+        title: "No"
+    },
+    {
+        icon: icon6,
+        title: "Move"
+    },
+    {
+        icon: icon7,
+        title: "Music"
+    },
+    {
+        icon: icon8,
+        title: "Drink"
+    }
+]
 
 const ImgCard = props => {
     return (
-        <div class="card">
-            <div class="container">
-                <Card>
-                    <CardImg top width="35%" src={icon2} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Goals</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText> </CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardImg top width="35%" src={icon} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle> Activity</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardImg top width="35%" src={icon3} alt=""></CardImg>
-                    <CardBody>
-                        <CardTitle>Home</CardTitle>
-                        <CardSubtitle></CardSubtitle>
-                        <CardText></CardText>
-                        <Button>Click</Button>
-                    </CardBody>
-                </Card>
-            </div>
+        <div class="container">
+            <Row>
+                {dummyArr.map(cardItem => {
+                    return (
+                        <Col md={3}>
+                            <Card>
+                                <CardImg top src={cardItem.icon} alt=""></CardImg>
+                                <CardBody>
+                                    <CardTitle>{cardItem.title}</CardTitle>
+                                    <CardSubtitle></CardSubtitle>
+                                    <CardText> </CardText>
+                                    <Button>Click</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
         </div>
     );
 };
