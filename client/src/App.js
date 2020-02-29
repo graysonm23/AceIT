01b6433adab543d55052fc31b6e01534c0bfcb03
Jude <judecarpenter@ymail.com>
2020-02-29 12:00:07 -0600

    changed sign up, added dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
-import NoMatch from "./pages/NoMatch";
+import NoMatch from "./Pages/NoMatch";
 import Nav from "./components/navbar";
 import Card from "./components/card";
-import Settings from "./pages/NewUS";
-import Profile from "./pages/UserProfile";
+import Settings from "./Pages/NewUS";
+import Profile from "./Pages/UserProfile";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Signup from "./pages/signup";
+import Signup from "./Pages/signup";
+import Home from "./Pages/NewUS";
+import dashboard from "./Pages/dashboard";
 
 function App() {
   return (
@@ -17,10 +19,10 @@ function App() {
         <Switch>
           <Route exact path="/card" component={Card} />
           <Route exact path="/signup" component={Signup} />
-
+          <Route exact path="/" component={Home} />
           <Route exact path="/settings" component={Settings} />
           <Route exact path="/userprofile" component={Profile} />
-          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/dashboard" component={dashboard} />
           <Route component={NoMatch} />
         </Switch>
       </div>
