735bad61416c7da87af983d808599a08cce1e9d8
Grayson M <grayson.mcmurry23@gmail.com>
2020-02-29 14:02:31 -0600

    adding local changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,12 @@ import NoMatch from "./pages/NoMatch";
 import Nav from "./components/navbar";
 import Login from "./components/login";
 import Card from "./components/card";
-<<<<<<< HEAD
-
-import "bootstrap/dist/css/bootstrap.min.css";
-=======
 import Settings from "./pages/NewUS";
 import Profile from "./pages/UserProfile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./pages/signup";
 import Home from "./pages/NewUS";
 import dashboard from "./pages/dashboard";
->>>>>>> 452d966c00f848a2d7c67b7a12f897d457564aa7
 
 function App() {
   return (
@@ -23,16 +18,10 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/card" component={Card} />
-<<<<<<< HEAD
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/" component={Home} />
-=======
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/settings" component={Settings} />
           <Route exact path="/userprofile" component={Profile} />
           <Route exact path="/dashboard" component={dashboard} />
->>>>>>> 452d966c00f848a2d7c67b7a12f897d457564aa7
           <Route component={NoMatch} />
         </Switch>
       </div>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -19,7 +19,6 @@ import API from "../utils/API";
 
 function Signup() {
   const [email, setEmail] = useState([]);
-<<<<<<< HEAD
   const [password, setPassword] = useState([]);
   const handleEmailSubmit = event => {
     event.preventDefault();
@@ -29,17 +28,12 @@ function Signup() {
     const userObj = {
       email: email,
       password: password
-    }
+    };
     API.signUpRoute(userObj)
       .then(res => {
         console.log(res);
       })
       .catch(err => console.log("Unable to save email ", err));
-=======
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(event);
->>>>>>> 452d966c00f848a2d7c67b7a12f897d457564aa7
   };
   return (
     <div className="homepage">
@@ -52,71 +46,8 @@ function Signup() {
                   <h1 className="signupTitle">Signup</h1>
                 </CardTitle>
                 <CardBody className="homeCardBody">
-<<<<<<< HEAD
-                  <form onSubmit={handleEmailSubmit}>
-                    <FormGroup>
-                      <label for="Your Name">
-                        <h4 className="signupSubtitle">Enter your Name</h4>{" "}
-                      </label>
-                      <input
-                        className="col s-12 signup-input"
-                        type="Name"
-                        name="name"
-                        id="exampleName"
-                        placeholder="John Doe"
-                      />
-                    </FormGroup>
-                    <FormGroup>
-                      <label for="Your Email">
-                        <h4 className="signupSubtitle">Enter your Email</h4>{" "}
-                      </label>
-                      <input
-                        onChange={e => setEmail(e.target.value)}
-                        value={email}
-                        className="col s-12 signup-input"
-                        type="email"
-                        name="email"
-                        id="exampleEmail"
-                        placeholder="example@example.com"
-                      />
-                    </FormGroup>
-                    <br></br>
-                    <FormGroup>
-                      <label for="Your Email">
-                        <h4 className="signupSubtitle">Enter your Password</h4>{" "}
-                      </label>
-                      <input
-                        onChange={e => setPassword(e.target.value)}
-                        className="col s-12 signup-input"
-                        type="password"
-                        name="ExamplePassword"
-                        id="examplePassword"
-                        placeholder="Your Password Here"
-                      />
-                      <br></br>
-                      {/* <input
-                        className="col s-12 signup-input"
-                        type="confirmPassword"
-                        name="ConfirmPassword"
-                        id="CondirmPassword"
-                        placeholder="Confirm Password"
-                      /> */}
-                    </FormGroup>
-                    <Button id="buttonCancel" size="lg" color="danger">
-                      X
-                    </Button>{" "}
-                    <Button
-                      color="success"
-                      id="buttonSubmit"
-                      size="lg"
-                      onClick={handleEmailSubmit}
-                    >
-                      ✓
-                    </Button>
-=======
                   <form>
                     <ValidatedSignupForm />
->>>>>>> 452d966c00f848a2d7c67b7a12f897d457564aa7
                   </form>
                 </CardBody>
               </CardBody>
